d2bdb1459e594052c55b2202b728f6a82221f93d
Arshdeep-nayan <arshdeepnayan@gmail.com>
2026-07-22 07:46:11 +0530

    config file setup for ai-screening-service

---
+++ AI-screening-service/src/main/java/com/smarthire/AI_screening_service/config/ChatClientConfig.java
@@ -0,0 +1,17 @@
+package com.smarthire.AI_screening_service.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(
+            GoogleGenAiChatModel chatModel) {
+
+        return ChatClient.create(chatModel);
+    }
+}
